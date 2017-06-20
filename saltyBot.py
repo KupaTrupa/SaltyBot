012dc90134a89d65fbf78705da675b3e7234d44b
KupaTrupa <paratroopa50@gmail.com>
2017-06-20 17:51:52 +0200

    Ajout reload fichier config

--- saltyBot.py
+++ saltyBot.py
@@ -19,10 +19,15 @@ class UnauthorizedError(Exception):
     pass
 
 
-config_file = open('config.json', 'r+')
-config_json = json.load(config_file)
-
-client = discord.Client()
+def reload_config(old_config = None):
+    config = {}
+    
+    if old_config is not None and "file" in old_config:
+        old_config["file"].close()
+    config["file"] = open('config.json', 'r+')
+    
+    config["json"] = json.load(config["file"])
+    return config
 
 def get_role_index(server, role_name):
     role_list = list(map(lambda x: str(x), server.roles))
@@ -30,15 +35,17 @@ def get_role_index(server, role_name):
     role_index = role_list.index(role_name) if role_name in role_list else None
     return role_index
 
+def check_if_authorized(user_roles, authorized_roles):
+    is_authorized = False
+    for role_id in user_roles:
+        if role_id in authorized_roles:
+            is_authorized = True
+            break
+    return is_authorized
+
 async def kick_member(message):
     try:
-        is_authorized = False
-        for role_id in list(map(lambda x: x.id, message.author.roles)):
-            if role_id in config_json["moderatorRanks"]:
-                is_authorized = True
-                break
-
-        if not is_authorized:
+        if not check_if_authorized([role.id for role in message.author.roles], config["json"]["moderatorRoles"]):
             raise UnauthorizedError("Not authorized (Kick command)")
         
         args_match = re.match(r"!kick ([\w]+#[0-9]{4}|\"[\w ]+#[0-9]{4}\")( *[0-9]*)$",
@@ -60,7 +67,7 @@ async def kick_member(message):
         member_name = args_match.group(1).replace('"', '')
         member_to_kick = message.server.get_member_named(member_name)
 
-        role_name = "Kicked from " + str(message.channel)
+        role_name = "Kicked from {}".format(str(message.channel))
 
         role_index = get_role_index(message.channel.server, role_name)
 
@@ -71,11 +78,11 @@ async def kick_member(message):
         # On ajoute le rôle de kické du chan au batar
 
         await client.add_roles(member_to_kick, message.channel.server.roles[role_index])
-        await client.send_message(message.channel, member_name + " a été kick du salon pour " +
-                                  str(kick_duration) + " minute(s).")
+        await client.send_message(message.channel, "{} a été kick du salon pour {} minute(s)."
+                                  .format(member_name, str(kick_duration)))
         
-        print("[" + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "] Kick : " +
-              member_name + " (" + str(kick_duration) + " minute(s))")
+        print("[{}] Kick : {} ({} minute(s))".format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                                                     member_name, str(kick_duration)))
 
         # Attente fin kick & on enlève le rôle au kické
 
@@ -91,6 +98,10 @@ async def kick_member(message):
     except IncorrectArgumentTypeError:
         await client.send_message(message.channel, "Le temps d'exclusion doit être un entier positif")
 
+config = reload_config()
+
+client = discord.Client()
+
 @client.event
 async def on_ready():
     channels = client.get_all_channels()
@@ -104,7 +115,7 @@ async def on_ready():
                 roles_list[role.id] = str(role)
         
         if str(channel.type) == 'voice':
-            role_name = 'Kicked from ' + str(channel)
+            role_name = 'Kicked from {}'.format(str(channel))
             role_index = get_role_index(channel.server, role_name)
             
             if role_index is None:
@@ -114,7 +125,7 @@ async def on_ready():
             chan_perms = channel.overwrites_for(channel.server.roles[role_index])
             chan_perms.update(connect=False)
         else:
-            role_name = 'Kicked from ' + str(channel)
+            role_name = 'Kicked from {}'.format(str(channel))
             role_index = get_role_index(channel.server, role_name)
             
             if role_index is None:
@@ -135,7 +146,16 @@ async def on_message(message):
         await client.send_message(message.channel, 'PONG !')
     elif message.content.startswith('!kick'):
         await kick_member(message)
+    elif message.content.startswith("!reload"):
+        try:
+            global config
+            if not check_if_authorized([role.id for role in message.author.roles], config["json"]["moderatorRoles"]):
+                raise UnauthorizedError("Not authorized (Kick command)")
+
+            config = reload_config(config)
+        except UnauthorizedError:
+            await client.send_message(message.channel, "Vous n'êtes pas autorisé à utiliser cette commande")
     elif message.content.startswith('!close'):
         await client.close()
 
-client.run(config_json["apiKey"])
+client.run(config["json"]["apiKey"])
